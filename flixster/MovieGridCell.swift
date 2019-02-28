b048d8e7374f0f24f4abd9f8b31ac8c3a7c8fe7c
kev-th <kevinkthaw@gmail.com>
2019-02-28 15:55:07 -0500

    Add part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kevin Thaw on 2/24/19.
+//  Copyright © 2019 Kevin Thaw. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
